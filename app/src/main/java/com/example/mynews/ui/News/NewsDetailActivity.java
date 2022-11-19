f2490c64799038cbdc33ce95f16b7d8f839b57af
Jatin <coderjatin123@gmail.com>
2022-11-19 13:31:42 +0530

    news detail send done.

--- app/src/main/java/com/example/mynews/ui/News/NewsDetailActivity.java
+++ app/src/main/java/com/example/mynews/ui/News/NewsDetailActivity.java
@@ -1,29 +1,32 @@
 package com.example.mynews.ui.News;
 
 import android.content.Intent;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.bumptech.glide.Glide;
-import com.example.mynews.Model_News;
 import com.example.mynews.R;
-import org.w3c.dom.Text;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Date;
 
 public class NewsDetailActivity extends AppCompatActivity {
     private ImageView imageView;
 
-    private TextView title;
+    private TextView title_view,time_view,desc_view,auth_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
         imageView=findViewById(R.id.img);
-        title=findViewById(R.id.d_title);
-
+        title_view=findViewById(R.id.d_title);
+        time_view=findViewById(R.id.d_time);
+        auth_view=findViewById(R.id.d_auth);
+        desc_view=findViewById(R.id.d_desc);
+        Date ts = null;
 
         Intent intent=getIntent();
 
@@ -31,10 +34,20 @@ public class NewsDetailActivity extends AppCompatActivity {
         String desc=intent.getStringExtra("desc");
         String url=intent.getStringExtra("img");
         String auth=intent.getStringExtra("auth");
+        auth="Author : "+auth;
         String time=intent.getStringExtra("time");
 
         Glide.with(this).load(url).into(imageView);
-        this.title.setText(title);
+        title_view.setText(title);
+
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            ts = Timestamp.from( Instant.parse(time) );
+        }
+        time_view.setText(String.valueOf(ts));
+        desc=desc.replaceAll("\\s+$","");
+        desc_view.setText(desc);
+        auth_view.setText(auth);
 
 
     }
